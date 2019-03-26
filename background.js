db5f2cb45a1ba74a0b46f713da1263c4121360f2
Omid Nikrah <omidnikrah@gmail.com>
2019-03-27 00:44:36 +0430

    init commit 🙈

---
+++ background.js
@@ -0,0 +1,20 @@
+let isActive = false;
+chrome.browserAction.setBadgeBackgroundColor({ color: isActive ? '#0097ff' : '#777' });
+chrome.browserAction.setBadgeText({
+	text: isActive ? 'On' : 'Off'
+});
+chrome.webRequest.onBeforeRequest.addListener(
+	() => ({
+		cancel: isActive
+	}),
+	{ urls: [ '*://*.instagram.com/stories/reel/seen*' ] },
+	[ 'blocking' ]
+);
+
+chrome.browserAction.onClicked.addListener(() => {
+	isActive = !isActive;
+	chrome.browserAction.setBadgeBackgroundColor({ color: isActive ? '#0097ff' : '#777' });
+	chrome.browserAction.setBadgeText({
+		text: isActive ? 'On' : 'Off'
+	});
+});
